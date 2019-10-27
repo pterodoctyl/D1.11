075d6879f382e0f1e43c1d2eaae18185bc7579d1
pterodoctyl <52787519+pterodoctyl@users.noreply.github.com>
2019-10-27 14:50:43 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+import setuptools  
+  
+with open("README.md", "r") as fh:  
+ long_description = fh.read()  
+setuptools.setup(  
+ name="trello_client-basics-api-pterodoktil", version="0.0.1", author="pterodoktil", author_email="lucenko.1998@list.ru", description="testestestest", long_description=long_description, long_description_content_type="text/markdown", url="[https://https://github.com/pterodoctyl/D1_trello_client](https://https://github.com/pterodoctyl/D1_trello_client)", 
